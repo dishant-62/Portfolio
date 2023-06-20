91560985f54c47c178ef506293e219697298b232
Dishant <rathidishant39@gmail.com>
2023-06-20 20:12:02 +0530

    My first professional comment on Github

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./components/header/header";
+
+function App() {
+  return (
+    <>
+    <Header/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const header = () => {
+  return (
+    <>
+    <nav className="nav container">
+        
+
+    </nav>
+
+    </>
+  )
+}
+
+export default header;
