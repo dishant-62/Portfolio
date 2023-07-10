c84abd6a779da9e97fbd9362f6e2a855dc48d3cb
Dishant <rathidishant39@gmail.com>
2023-07-10 13:13:38 +0530

    last

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
       <h2 className="section__title">Get in Touch</h2>
       <span className="section__subtitle">Contact Me</span>
 
-      <div className="contact__container container grid">
+      <div className="contact__container grid">
         <div className="contact__content">
           <h3 className="contact__title">Talk to me</h3>
 
